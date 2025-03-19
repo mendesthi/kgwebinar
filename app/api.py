8506c0b72adecc7604d6c78ee8d96e87c28b771d
thiago-mendes_sap <thiago.mendes@sap.com>
2025-03-19 11:07:50 +0100

    Enhance translate_nl_to_sparql function to include properties and classes queries, and update prompt template for SPARQL query generation

--- app/api.py
+++ app/api.py
@@ -80,38 +80,133 @@ def execute_sparql_query():
 
 @app.route('/translate_nl_to_sparql', methods=['POST'])
 def translate_nl_to_sparql():
-    try:
+    try:    
         # Get the natural language query and ontology from the request body
         data = request.get_json()
         nl_query = data.get('nl_query')
-        ontology = data.get('ontology')
+        ontology_query = data.get('ontology')
+        properties_query = data.get('properties')
+        classes_query = data.get('classes')
+        
+        if not nl_query:
+            return jsonify({'error': 'Natural language query and ontology query are required'}), 400
 
-        if not nl_query or not ontology:
-            return jsonify({'error': 'Natural language query and ontology are required'}), 400
+        # Get the ontology needed
+        ontology_query = "CONSTRUCT {?s ?p ?o} FROM <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology-v2> WHERE {?s ?p ?o}"
 
+        # GET ONTOLOGY - Directly call the logic of execute_sparql_query
+        cursor = connection.connection.cursor()
+
+        result = cursor.callproc('SPARQL_EXECUTE', (ontology_query, 'application/sparql-results+csv', '?', '?'))
+        ontology = result[2]
+
+        # Get the properties needed
+        property_query = """
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix : <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology-v2/> 
+            prefix owl: <http://www.w3.org/2002/07/owl#>
+            prefix foaf:  <http://xmlns.com/foaf/0.1/>
+            
+            SELECT DISTINCT ?property_iri ?domain ?range ?description 
+            FROM <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology-v2> 
+            WHERE { 
+                {?property_iri rdf:type owl:ObjectProperty .}
+                UNION
+                {?property_iri rdf:type owl:DatatypeProperty .}
+                OPTIONAL {?property_iri rdfs:comment ?description .}
+                OPTIONAL {
+                    ?property_iri rdfs:domain ?domain ;
+                        rdfs:range ?range .}
+            }
+            ORDER BY ?property_iri
+        """
+
+        # GET PROPERTIES - Directly call the logic of execute_sparql_query
+        cursor = connection.connection.cursor()
+
+        result = cursor.callproc('SPARQL_EXECUTE', (property_query, 'application/sparql-results+json', '?', '?'))
+        properties = result[2]
+        
+        # Get the classes needed
+        classes_query = """
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix : <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology-v2/> 
+            prefix owl: <http://www.w3.org/2002/07/owl#>
+            prefix foaf:  <http://xmlns.com/foaf/0.1/>
+
+            SELECT DISTINCT ?class_iri ?description
+            FROM <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology-v2> 
+            WHERE { 
+                ?class_iri rdf:type owl:Class . 
+                FILTER (isIRI(?class_iri)) . 
+                OPTIONAL { ?class_iri rdfs:comment ?description } 
+            }
+            ORDER BY ?class_iri
+        """
+        
+        # GET CLASSES - Directly call the logic of execute_sparql_query
+        cursor = connection.connection.cursor()
+
+        result = cursor.callproc('SPARQL_EXECUTE', (classes_query, 'application/sparql-results+json', '?', '?'))
+        classes = result[0]
+        
+        instructions = """
+            When you generate the final query, remove these ``` quotes and only return the query.
+        """
+
+        prefixes = """ 
+            Use the following prefixes when generating the SPARQL query:
+            
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix : <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology/> 
+            prefix owl: <http://www.w3.org/2002/07/owl#>
+            prefix foaf:  <http://xmlns.com/foaf/0.1/>
+        """
+        graph = """http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-rdf-v4"""
+        graph_inferred = """http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-inferred-triples-v4"""
+
+        query_example = """
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix : <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-ontology/> 
+            prefix owl: <http://www.w3.org/2002/07/owl#>
+            prefix foaf:  <http://xmlns.com/foaf/0.1/>
+            
+            SELECT ?partnerRef ?psrRef
+            FROM 
+                <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-rdf-v4> 
+            FROM 
+                <http://www.semanticweb.org/ontologies/2025/smart-technical-advisory-inferred-triples3>
+            WHERE 
+            {  
+                ?partnerRef a :SAPPartner;
+                    :requested ?psrRef .
+            }
+        """
         # Initialize the LLM model from SAP AI Hub
         llm = init_llm(model_name="gpt-4o")
 
         # Define the prompt template
         prompt_template = PromptTemplate(
-            input_variables=["nl_query", "ontology"],
+            input_variables=["nl_query", "classes", "properties", "ontology", "graph", "graph_inferred", "prefixes", "query_example", "instructions"],
             template="""
-            You are an expert in SPARQL queries. Given the following ontology and a user's question in natural language, generate a valid SPARQL query.
+            Use the provided information about the classes and properties from the ontology 
+            to generate a SPARQL query corresponding to the request here: {nl_query} .
 
-            Ontology:
-            - Class: SAPPartner (has properties: accountName, partnerStatus, ofCountry)
-            - Class: Country (has properties: countryName, belongsToSAPRegion)
-            - Class: SAPRegion (has properties: regionName)
-            - Relationship: ofCountry (SAPPartner → Country)
-            - Relationship: belongsToSAPRegion (Country → SAPRegion)
-            - Property: accountName (SAPPartner → String)
-            - Property: partnerStatus (SAPPartner → String)
-            - Property: countryName (Country → String)
-            - Property: regionName (SAPRegion → String)
+            Information about the classes defined in the ontology can be found here in csv format: {classes} .
+            Information about the properties defined in the ontology can be found here in csv format: {properties} . 
+            Check always the domain and range of the properties to understand how to build the SPARQL query.
+            The whole ontology is described in turtle format here: {ontology} .
 
-            User's Question: {nl_query}
+            When you build the SPARQL query use the following prefixes: {prefixes} and refer to this example: {query_example} .
+            The knowledge graphs to query are: {graph} and {graph_inferred} . Always include them in two FROM statements as in the provided example.
+            If the request asks for information about PBC order numbers, always name the variable "pcbOrderNumber" .
+            Always access the PBC order numbers with corresponding property of the service class.
 
-            SPARQL Query:
+            Consider also the following final instructions: {instructions} .
             """
         )
 
@@ -119,7 +214,7 @@ def translate_nl_to_sparql():
         chain = LLMChain(llm=llm, prompt=prompt_template)
 
         # Run the chain with the provided inputs
-        response = chain.run({"nl_query": nl_query, "ontology": ontology})
+        response = chain.run({"nl_query": nl_query, "classes":classes, "properties": properties, "ontology": ontology, "graph":graph, "graph_inferred":graph_inferred, "prefixes":prefixes, "query_example":query_example, "instructions":instructions})
 
         sparql_query = response.strip()
 
